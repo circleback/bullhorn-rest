af1cfa5d6b5a0611e29eab32cc88a339d110a380
David Patnode <draknor@gmail.com>
2015-03-28 08:26:38 -0500

    Debugging settings entity

--- lib/bullhorn/rest/entities/setting.rb
+++ lib/bullhorn/rest/entities/setting.rb
@@ -5,7 +5,15 @@ module Entities
 module Setting
   extend Base
 
-  define_methods(:immutable => true)
+  # define_methods(:immutable => true)
+
+  define_method('settings') do |names = []|
+	names = names.empty? ? '*' : names.join(',')
+	path = "settings/#{names}"
+	res = conn.get path, params
+	Hashie::Mash.new JSON.parse(res.body)
+  end
+
 end
 
 end
