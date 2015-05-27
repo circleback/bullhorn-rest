f92d0529577c5fc7eded58be78f75e3e45b0cb49
David Patnode <draknor@gmail.com>
2015-05-27 15:57:34 -0500

    Use different orderBy field since job_order has no 'name'

--- lib/bullhorn/rest/entities/job_order.rb
+++ lib/bullhorn/rest/entities/job_order.rb
@@ -6,6 +6,15 @@ module JobOrder
   extend Base
 
   define_methods(:owner_methods => true)
+
+  def query_job_orders(options={})
+    params = {:fields => '*', :count => '500', :orderBy => 'title'}.merge(options)
+    path = "query/JobOrder"
+    res = @conn.get path, params
+    obj = decorate_response JSON.parse(res.body)
+    attach_next_page obj, options, path, conn
+  end
+
 end
 
 end
