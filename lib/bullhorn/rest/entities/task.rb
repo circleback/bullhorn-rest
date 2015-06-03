4a2b64aa166f1ac97835ae9e63ee916bf10570c6
David Patnode <draknor@gmail.com>
2015-06-03 14:20:07 -0500

    Fix query_tasks

--- lib/bullhorn/rest/entities/task.rb
+++ lib/bullhorn/rest/entities/task.rb
@@ -6,6 +6,15 @@ module Task
   extend Base
 
   define_methods
+
+  def query_tasks(options={})
+    params = {:fields => '*', :count => '500', :orderBy => 'dateEnd'}.merge(options)
+    path = "query/Task"
+    res = @conn.get path, params
+    obj = decorate_response JSON.parse(res.body)
+    attach_next_page obj, options, path, conn
+  end
+
 end
 
 end
